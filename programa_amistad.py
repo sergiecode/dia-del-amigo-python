dc07cb3378d72dd3b2919717ff3681c4259381da
SnSDk <sergiosnsdk@gmail.com>
2023-07-20 08:50:52 -0300

    dia-del-amigo-python

---
+++ programa_amistad.py
@@ -0,0 +1,8 @@
+def mensaje_amistad(nombre1, nombre2):
+    mensaje = f"La amistad entre {nombre1} y {nombre2} es como un código bien estructurado, ¡siempre se complementan!"
+    return mensaje
+
+nombre1 = input("Ingresa el primer nombre: ")
+nombre2 = input("Ingresa el segundo nombre: ")
+
+print(mensaje_amistad(nombre1, nombre2))
